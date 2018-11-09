ffc0357e08a930b168ff0eb06c74170907fd13fb
94kamal <94kamal@gmail.com>
2018-11-09 21:23:07 +0530

    14

--- src/main/java/com/laundrovalley/rest/model/Wash.java
+++ src/main/java/com/laundrovalley/rest/model/Wash.java
@@ -2,4 +2,12 @@ package com.laundrovalley.rest.model;
 
 public class Wash {
 private int washid;
+
+public int getWashid() {
+	return washid;
+}
+
+public void setWashid(int washid) {
+	this.washid = washid;
+}
 }
